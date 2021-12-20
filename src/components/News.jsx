14890384417597e5c9ece09a481c8ff823b8a664
Amjad Yahia <amjedyehia1998@gmail.com>
2021-12-20 13:49:00 +0200

    add key props to list items

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -44,7 +44,9 @@ const News = ({ simplified }) => {
             }>
             <Option value='Cryptocurrency'>Cryptocurrency</Option>
             {data?.data?.coins.map((coin) => (
-              <Option value={coin.name}>{coin.name}</Option>
+              <Option key={coin.name} value={coin.name}>
+                {coin.name}
+              </Option>
             ))}
           </Select>
         </Col>
